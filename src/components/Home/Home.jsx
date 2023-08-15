2b4cd35f751e732b7f31c74025e16e260a1787f4
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-15 22:59:16 +0600

    feat: About component added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import About from './Main/About/About';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <About></About>
         </div>
     );
 };
