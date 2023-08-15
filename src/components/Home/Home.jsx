67460f87f8da22953bfe10ebacd8cc663898cab0
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-16 00:05:56 +0600

    feat: Staffs Component added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
 import About from './Main/About/About';
+import Staffs from './Main/Staffs/Staffs';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Staffs></Staffs>
             <About></About>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Home />
+        element: <Home />,
+        loader: () => fetch('fakeData/schoolsInfo.json')
       }
     ]
   }
